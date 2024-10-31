0550255e786b067bf0c46f1601f49a70bc63e65b
Gasegamer <gasegamer@gmail.com>
2024-10-31 10:03:00 -0300

    Create lista3_ex26.c

---
+++ Lista 3/Códigos/lista3_ex26.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct
+{
+  char nome[50];
+  char disciplina[100];
+  int salario;
+} Professor;
+
+
+int main()
+{
+  Professor professor;
+
+  strcpy(professor.nome, "Roberto");
+  strcpy(professor.disciplina, "ATP");
+
+  professor.salario = 2000;
+
+  printf("Nome: %s\nDiscpilina: %s\nSalario: %d\n",
+    professor.nome,
+    professor.disciplina,
+    professor.salario
+  );
+
+  return 0;
+}
